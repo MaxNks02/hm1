1f51063edb21a431883a3775a8e7916a2ac02507
maman <maman.nks2408@gmail.com>
2024-07-06 23:26:45 +0300

    commit hm5

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,17 @@ import Comic from './components/Comic';
 import ZakyatCalculator from './components/ZakyatCalculator';
 import MarketSite from './components/Market';
 
-
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Projects />} />
-                <Route path="/comic-identifier" element={<Comic />} />
-                <Route path="/project2" element={<ZakyatCalculator />} />
-                <Route path="/project3" element={<MarketSite />} />
-                {/* Add more routes as needed */}
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Projects />} />
+        <Route path="/comic-identifier" element={<Comic />} />
+        <Route path="/project2" element={<ZakyatCalculator />} />
+        <Route path="/project3" element={<MarketSite />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
 };
 export default App;
--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import '../styles/Bio.css'
+import '../styles/Bio.css';
 
 const Bio: React.FC = () => {
-    return (
-        <section>
-            <h2>About Me</h2>
-            <p>Hello! I'm Maman Torebaev. I am a young developer and student from Innopolis.</p>
-        </section>
-    );
+  return (
+    <section className="bio">
+      <h2 className="bio h2">About Me</h2>
+      <p className="bio p">
+        Hello! I am Maman Torebaev. I am a young developer and student from
+        Innopolis.
+      </p>
+    </section>
+  );
 };
 
 export default Bio;
